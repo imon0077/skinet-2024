b309c0689dbe421d51e9312aac4b05f16c430154
Imon Islam <imon0077@yahoo.com>
2024-10-15 19:08:14 +0600

    Repository pattern implemented for product, run migration and product import from json file during project startup, get brand and types also filtering and sorting features implemented | ES3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,19 +8,19 @@ namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductsController(StoreContext context) : ControllerBase
+    public class ProductsController(IProductRepository repo) : ControllerBase
     {
-        private readonly StoreContext context = context;
+        //private readonly StoreContext context = context;
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort)
         {
-            return await context.Products.ToListAsync();
+            return Ok(await repo.GetProductsAsync(brand, type, sort));
         }
 
         [HttpGet("{id:int}")]
         public async Task<ActionResult<Product>> GetProduct(int id){
-            var product = await context.Products.FindAsync(id);
+            var product = await repo.GetProductByIdAsync(id);
 
             if(product == null) return NotFound();
 
@@ -29,10 +30,12 @@ namespace API.Controllers
         [HttpPost]
         public async Task<ActionResult<Product>> CreateProduct(Product product)
         {
-            context.Products.Add(product);
-            await context.SaveChangesAsync();
+            repo.AddProduct(product);
+            if(await repo.SaveChangesAsync()){
+                return CreatedAtAction("GetProduct", new{id = product.Id}, product);
+            }
 
-            return product;
+            return BadRequest("Problem Creating Product");
         }
 
         [HttpPut("{id:int}")]
@@ -41,26 +44,41 @@ namespace API.Controllers
             if(product.Id != id || !ProductExists(id))
                 return BadRequest("Cannot update this product.");
 
-            context.Entry(product).State = EntityState.Modified;
-            await context.SaveChangesAsync();
+            repo.UpdateProduct(product);
 
-            return NoContent();
+            if(await repo.SaveChangesAsync()){
+                return NoContent();
+            }
+
+            return BadRequest("Problem updating product");
         }
 
         [HttpDelete("{id:int}")]
         public async Task<ActionResult> DeleteProduct(int id)
         {
-            var product = await context.Products.FindAsync(id);
+            var product = await repo.GetProductByIdAsync(id);
             if(product == null) return NotFound();
 
-            context.Products.Remove(product);
-            await context.SaveChangesAsync();
+            repo.DeleteProduct(product);
+            if(await repo.SaveChangesAsync()){
+                return NoContent();
+            }
 
-            return NoContent();
+            return BadRequest("Problem deleting product");
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<string>>> GetBrands(){
+            return Ok(await repo.GetBrandAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<string>>> GetTypes(){
+            return Ok(await repo.GetTypesAsync());
         }
 
         private bool ProductExists(int id){
-            return context.Products.Any(x => x.Id == id);
+            return repo.ProductExists(id);
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(opt => {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
@@ -24,5 +26,19 @@ var app = builder.Build();
 // app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    throw;
+}
 
 app.Run();
